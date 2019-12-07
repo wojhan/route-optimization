9407b13b19b5191d63ff587716afa9259b9f4770
wojhan <w.haniewski@gmail.com>
2019-12-07 21:11:36 +0100

    Employee - Crud operations on angular

--- django/data/urls.py
+++ django/data/urls.py
@@ -1,15 +1,18 @@
 from rest_framework import routers
 
 from data.viewsets import (BusinessTripViewSet, CompanyViewSet,
-                           CurrentUserView, ObtainUserFromTokenView,
-                           RequistionViewSet, UserViewSet)
-from django.conf.urls import url, include
+                           CurrentUserView, EmployeeViewSet,
+                           ObtainUserFromTokenView, RequistionViewSet,
+                           UserViewSet)
+from django.conf.urls import include, url
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.SimpleRouter()
 router.register(r'api/users', UserViewSet)
 router.register(r'api/companies', CompanyViewSet)
 router.register(r'api/business-trips', BusinessTripViewSet)
 router.register(r'api/requistions', RequistionViewSet)
+router.register(r'api/employees', EmployeeViewSet, 'employee')
 
 urlpatterns = router.urls
 
